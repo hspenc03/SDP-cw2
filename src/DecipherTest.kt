a0df25be6a6dab17cdf16059b8ef7a0eb69bc486
Hannah <hannahlspencer@gmail.com>
2018-02-24 15:35:24 +0000

    add grouping and offset tests

--- src/DecipherTest.kt
+++ src/DecipherTest.kt
@@ -1,5 +1,6 @@
 import org.junit.*
 import kotlin.test.assertEquals
+import kotlin.test.expect
 
 class DecipherTest {
     lateinit var deciphering: Caeser
@@ -19,6 +20,20 @@ class DecipherTest {
     fun testCleanString() {
         val expected = "thisisconfusing"
         assertEquals(expected, deciphering.cleanString("this is confusing!"))
+    }
+
+    @Test
+    fun testGroupingCounter() {
+        val word = "butter"
+        val expected = mapOf('b' to 1, 'u' to 1, 't' to 2, 'e' to 1, 'r' to 1)
+        assertEquals(expected, deciphering.groupingFrequency(word))
+    }
+
+    @Test
+    fun testFindOffset() {
+        val word = "eeeeeeeeettttaaa"
+        val expected = 9
+        assertEquals(expected, deciphering.findOffset(word))
     }
 
 
