dc1f85a2e8bc7783f03f747e2adb7bf0afd71d7b
essvee <s.vincent@nhm.ac.uk>
2018-02-24 13:27:47 +0000

    recursion working

--- src/practice.kt
+++ src/practice.kt
@@ -1,27 +1,39 @@
 class practice {
     fun encipher(s: String, n: Int): String {
-        if(n < 0 || n > 25 || n !is Int || s !is String) {
-            throw IllegalArgumentException()
-        } //regex to check string
+        var cipheredString = ""
 
-        var modifiedS = s;
-        if (s == "") {
-            //move by 1
-            //return
+        if(n < 0 || n > 25) {
+            throw IllegalArgumentException()
+        }
+        var charArray = s.toCharArray()
+
+        // Base case - return if last character in input string
+        if (s.length == 1) {
+            var offsetChar = helperFunction(s.toCharArray()[0], n).toString()
+            return offsetChar
+        } else {
+            var offsetChar = helperFunction(s.toCharArray()[0], n).toString()
+            cipheredString += helperFunction(charArray[0], n)
+
         }
 
-        return (encipher(modifiedS, n))
+        cipheredString += encipher(s.substring(1), n)
+        return cipheredString
     }
 
     fun helperFunction(c: Char, n: Int): Char {
+        if(!c.isLetter()) {
+            throw IllegalArgumentException()
+        }
+
         var limit = 0
-        when(c.isLowerCase()) {
-            true -> limit = 122
-            false -> limit = 90
+        limit = when(c.isLowerCase()) {
+            true -> 122
+            false -> 90
         }
         var result = c + n
         if (result.toInt() > limit) {
-            result = result - 26
+            result -= 26
         }
         return result
     }
